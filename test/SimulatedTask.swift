5e4033a4fb600d26a16dd4ae06db40052f5ebc83
harrydw <harrydw@umich.edu>
2017-11-12 21:16:03 -0500

    Added functions for multiPeer Connectivity

--- test/SimulatedTask.swift
+++ test/SimulatedTask.swift
@@ -17,6 +17,42 @@ class SimulatedTask: BaseTask {
         // So now, I think you will need to implement the MC Peer connectivity
         // framework methods in this class and call start/stopAdvertising() or any
         // other functions you want to measure power consumption in this method
+        
+        ///An object that handles searching for and finding other phones on the network.
+        var serviceBrowser: MCNearbyServiceBrowser!
+        
+        ///An object that handles broadcasting one's presence on the network.
+        var serviceAdvertiser: MCNearbyServiceAdvertiser!
+        
+        ///`true` if this object is currently browsing.
+        var isBrowsing: Bool = true
+        ///`true` if this object is currently advertising.
+        var isAdvertising: Bool = true
+        
+        //MARK: Convenience methods
+        ///Begins advertising the current peer on the network.
+        func startAdvertisingPeer() {
+            serviceAdvertiser.startAdvertisingPeer()
+            isAdvertising = true
+        }
+        
+        ///Stops advertising the current peer.
+        func stopAdvertisingPeer() {
+            serviceAdvertiser.stopAdvertisingPeer()
+            isAdvertising = false
+        }
+        
+        ///Begins browsing for other peers on the network.
+        func startBrowsingForPeers() {
+            serviceBrowser.startBrowsingForPeers()
+            isBrowsing = true
+        }
+        
+        ///Stops browsing for other peers.
+        func stopBrowsingForPeers() {
+            serviceBrowser.stopBrowsingForPeers()
+            isBrowsing = false
+        }
     }
     
 }
