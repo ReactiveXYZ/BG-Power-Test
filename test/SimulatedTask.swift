adc77340f87c879cf5b69a55c84cdc385b7979aa
harrydw <harrydw@umich.edu>
2017-11-12 22:32:03 -0500

    fixed bug with MCP

--- test/SimulatedTask.swift
+++ test/SimulatedTask.swift
@@ -11,6 +11,41 @@ import MultipeerConnectivity
 
 class SimulatedTask: BaseTask {
     
+    var serviceBrowser: MCNearbyServiceBrowser!
+    
+    ///An object that handles broadcasting one's presence on the network.
+    var serviceAdvertiser: MCNearbyServiceAdvertiser!
+    
+    ///`true` if this object is currently browsing.
+    var isBrowsing: Bool = true
+    ///`true` if this object is currently advertising.
+    var isAdvertising: Bool = true
+    
+    //MARK: Convenience methods
+    ///Begins advertising the current peer on the network.
+    func startAdvertisingPeer() {
+        serviceAdvertiser.startAdvertisingPeer()
+        isAdvertising = true
+    }
+    
+    ///Stops advertising the current peer.
+    func stopAdvertisingPeer() {
+        serviceAdvertiser.stopAdvertisingPeer()
+        isAdvertising = false
+    }
+    
+    ///Begins browsing for other peers on the network.
+    func startBrowsingForPeers() {
+        serviceBrowser.startBrowsingForPeers()
+        isBrowsing = true
+    }
+    
+    ///Stops browsing for other peers.
+    func stopBrowsingForPeers() {
+        serviceBrowser.stopBrowsingForPeers()
+        isBrowsing = false
+    }
+    
     override func runTask() -> Void {
         // TODO...
         // Basically this function will be called with the selected interval
@@ -20,40 +55,10 @@ class SimulatedTask: BaseTask {
         // other functions you want to measure power consumption in this method
         
         ///An object that handles searching for and finding other phones on the network.
-        var serviceBrowser: MCNearbyServiceBrowser!
+        startAdvertisingPeer();
+        startBrowsingForPeers();
         
-        ///An object that handles broadcasting one's presence on the network.
-        var serviceAdvertiser: MCNearbyServiceAdvertiser!
         
-        ///`true` if this object is currently browsing.
-        var isBrowsing: Bool = true
-        ///`true` if this object is currently advertising.
-        var isAdvertising: Bool = true
-        
-        //MARK: Convenience methods
-        ///Begins advertising the current peer on the network.
-        func startAdvertisingPeer() {
-            serviceAdvertiser.startAdvertisingPeer()
-            isAdvertising = true
-        }
-        
-        ///Stops advertising the current peer.
-        func stopAdvertisingPeer() {
-            serviceAdvertiser.stopAdvertisingPeer()
-            isAdvertising = false
-        }
-        
-        ///Begins browsing for other peers on the network.
-        func startBrowsingForPeers() {
-            serviceBrowser.startBrowsingForPeers()
-            isBrowsing = true
-        }
-        
-        ///Stops browsing for other peers.
-        func stopBrowsingForPeers() {
-            serviceBrowser.stopBrowsingForPeers()
-            isBrowsing = false
-        }
     }
     
 }
